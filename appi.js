d278bd5ca7068e9d517c9dbb3385ac26cdb17b20
oscar morales <131509024+0skarmp@users.noreply.github.com>
2023-10-23 14:01:14 -0500

    Update api.js

--- appi.js
+++ appi.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'MYSQL$q1w2e3r4.',
-    database: 'tienda_abc'
+    // password: 'MYSQL$q1w2e3r4.',
+     database: 'Informacion'
 });
 
 connection.connect((error) => {
@@ -19,22 +20,33 @@ connection.connect((error) => {
 });
 
 
-app.get('/productos', (request, response) => {
+// app.get('/', (request, response) => {
 
-    connection.query(
-        "SELECT * FROM productos WHERE eliminado = 0 AND stock > 0;", 
-        (error, data) => {
-            if (error) {
-                console.error(error);
-                response.status(500).send("Error retrieving products");
-            } else {
-                response.send(data);
-            }
-        }
-    );
+//     connection.query(
+//         "SELECT * FROM productos WHERE eliminado = 0 AND stock > 0;", 
+//         (error, data) => {
+//             if (error) {
+//                 console.error(error);
+//                 response.status(500).send("Error retrieving products");
+//             } else {
+//                 response.send(data);
+//             }
+//         }
+//     );
 
-});
+app.post('/enviado', (request, response) => {
+    const { DNI, full_name, direccion, correo, telefono } = request.body;
+    connection.query('INSERT INTO Datos (DNI, full_name, direccion, correo, telefono) VALUES (?, ?, ?, ?)', [DNI, full_name, direccion, correo, telefono], (error) => {
+      if (error) {
+        console.error(error);
+        response.status(500).send('Error creating DataBase');
+      } else {
+        response.send('Database created successfully');
+      }
+    });
+  });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
