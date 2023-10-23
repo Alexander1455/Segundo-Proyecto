72b566e364a34d3dd0d655a7e13e420e4958452b
Jhony Arana <jharanza1986@gmail.com>
2023-10-23 14:57:00 -0500

    feat: Add the database

---
+++ database/formulario.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Informacion;
+
+USE Informacion;
+
+CREATE TABLE DATOS (
+  id 		int NOT NULL auto_increment,
+  DNI 		int NOT NULL,
+  full_name	varchar(50) NOT NULL,
+  direccion	varchar(250) NULL,
+  correo	varchar(150) NULL,
+  telefono	int NULL
+)
+
+INSERT INTO DATOS (DNI, full_name, direccion, correo, telefono )VALUES ( 12345678, 'Pepito Perez', 'Calle Arica 260 Miraflores Lima', 'pepito_perez@gmail.com', 999995555);
+
+DELETE FROM DATOS WHERE id=0;
